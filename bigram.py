fa88e056bff480f3e0f75f0e71be94f3dd451891
narz0001 <singa.gnarzary@gmail.com>
2024-07-15 01:37:21 +0800

    Added Feed Forward - Gives time for the heads to learn about the data.

--- bigram.py
+++ bigram.py
@@ -94,6 +94,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -104,6 +117,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.sa_heads = MultiHeadAttention(4,n_embd//4) # i.e. 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size) # language modelling head
 
     def forward(self, idx, targets=None):
@@ -113,7 +127,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (Batch = 32, Time, Channel)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_heads(x)
+        x = self.sa_heads(x) 
+        x = self.ffwd(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T, vocab_seize)
 
         if targets is None:
